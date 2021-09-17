f097f874066b6892b1df6d4ed043c77c0903988b
maksblink <blinkmaks777@gmail.com>
2021-09-17 22:49:23 +0200

    admin

--- final_project_django/final_project_app/admin.py
+++ final_project_django/final_project_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game, GameAnswers
+
+admin.site.register(Game)
+admin.site.register(GameAnswers)
--- final_project_django/final_project_app/models.py
+++ final_project_django/final_project_app/models.py
@@ -13,6 +13,9 @@ class Game(models.Model):
     is_game_ended = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return f"Operator: {self.operator}"
+
 
 class GameAnswers(models.Model):
     first_factor = models.IntegerField()
@@ -21,3 +24,6 @@ class GameAnswers(models.Model):
     correct_answer = models.IntegerField()
     was_this_answer_correct = models.BooleanField(null=True)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"First factor: {self.first_factor} Second factor: {self.first_factor}"
